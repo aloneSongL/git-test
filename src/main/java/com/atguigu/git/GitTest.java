8d9dc39749a98ed59a65e01c1da86b7617ca7d77
Sjx <1450320103@qq.com>
2022-05-04 08:58:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import sun.applet.Main;
+
+/**
+ * @author sjx
+ * @Description TODO
+ * @create 2022-05-04-8:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
