2ef830ae6bdee7de6bda9838080d2b56c7561a7f
Sjx <1450320103@qq.com>
2022-05-04 09:02:17 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ import sun.applet.Main;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
